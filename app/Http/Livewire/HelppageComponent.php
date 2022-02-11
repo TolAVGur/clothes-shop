f70bf9483c4c5ba384458caee75b0d871e21436e
Anatolii <gurov.anatoliy@gmail.com>
2022-02-11 12:43:44 +0200

    создана вьюха для helppage

---
+++ app/Http/Livewire/HelppageComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HelppageComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.helppage-component')->layout('layouts.base');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\ContactsComponent;
+use App\Http\Livewire\HelppageComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,4 @@ use App\Http\Livewire\ContactsComponent;
 Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/contacts', ContactsComponent::class);
+Route::get('/help', HelppageComponent::class);
