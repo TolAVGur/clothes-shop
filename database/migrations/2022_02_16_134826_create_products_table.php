d981058638c52985ad9a3c4af54ef40eb751b34b
Anatolii <gurov.anatoliy@gmail.com>
2022-02-17 15:39:04 +0200

    Доработаны модели Product, Category, Brand

--- database/migrations/2022_02_16_134826_create_products_table.php
+++ database/migrations/2022_02_16_134826_create_products_table.php
@@ -16,20 +16,24 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('short_description')->nullable();
-            $table->text('description');
-            $table->decimal('regular_price');
-            $table->decimal('sale_price')->nullable();
-            $table->string('SKU');
-            $table->enum('stock_status', ['instock', 'outofstock']);
-            $table->boolean('featured')->default(false);
-            $table->unsignedInteger('quantity')->default(10);
-            $table->string('image')->nullable();
-            $table->text('images')->nullable();
-            $table->bigInteger('category_id')->unsigned()->nullable();
-            $table->bigInteger('brand_id')->unsigned()->nullable();
+            $table->string('slug')->unique();                           // алеас/псевдоним
+            $table->string('short_description')->nullable();            // не обязательное свойство
+            $table->text('description');                                // описание
+            $table->decimal('sale_price');                              // розничная цена
+            $table->decimal('discount')->nullable();                    // скидка
+            $table->string('SKU');                                      // артикул-идентификатор
+            $table->enum('stock_status', ['instock', 'outofstock']);    // наличие на складе [есть/нет]
+            $table->boolean('featured')->default(false);                // эксклюзивный товар
+            $table->unsignedInteger('quantity')->default(10);           // количество товара по умолчанию 10 шт
+            $table->string('image')->nullable();                        // картинка не обяз
+            $table->text('images')->nullable();                         // колекция картинок, через "," (парсить по ",")
+            $table->bigInteger('category_id')->unsigned()->nullable();  // связь с категорией
+            $table->bigInteger('brand_id')->unsigned()->nullable();     // связь с брендом
             $table->timestamps();
+            // * связи
+            //$table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            //$table->foreign("brand_id")->references('id')->on('brands')->onDelete('cascade');
+
         });
     }
 
