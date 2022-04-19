f0c0e6aa8a6330f562742c55347f97cf24e3c06a
Anatolii <gurov.anatoliy@gmail.com>
2022-04-19 13:55:45 +0300

    Українізація шару livewire\card-component.blade.php

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -1,5 +1,5 @@
 @section('title')
-Корзина
+Кошик
 @endsection
 
 <section id="cart_items">
@@ -8,11 +8,11 @@
             <table class="table table-condensed">
                 <thead>
                     <tr class="cart_menu">
-                        <td class="image">Изделие</td>
+                        <td class="image">Виріб</td>
                         <td class="description"></td>
-                        <td class="price">Цена</td>
-                        <td class="quantity">Количество</td>
-                        <td class="total">Всего</td>
+                        <td class="price">Ціна</td>
+                        <td class="quantity">Кількість</td>
+                        <td class="total">Усього</td>
                         <td></td>
                     </tr>
                 </thead>
@@ -22,11 +22,11 @@
                             <a href=""><img src="{{ asset('images/cart/one.png') }}" alt=""></a>
                         </td>
                         <td class="cart_description">
-                            <h4><a href="">Colorblock Scuba</a></h4>
+                            <h4><a href="">тут короткий опис виробу</a></h4>
                             <p>Web ID: 1089772</p>
                         </td>
                         <td class="cart_price">
-                            <p>$59</p>
+                            <p>500 грн</p>
                         </td>
                         <td class="cart_quantity">
                             <div class="cart_quantity_button">
@@ -36,7 +36,7 @@
                             </div>
                         </td>
                         <td class="cart_total">
-                            <p class="cart_total_price">$59</p>
+                            <p class="cart_total_price">500 грн</p>
                         </td>
                         <td class="cart_delete">
                             <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
@@ -48,11 +48,11 @@
                             <a href=""><img src="{{ asset('images/cart/two.png') }}" alt=""></a>
                         </td>
                         <td class="cart_description">
-                            <h4><a href="">Colorblock Scuba</a></h4>
+                            <h4><a href="">тут короткий опис виробу</a></h4>
                             <p>Web ID: 1089772</p>
                         </td>
                         <td class="cart_price">
-                            <p>$59</p>
+                            <p>100 грн</p>
                         </td>
                         <td class="cart_quantity">
                             <div class="cart_quantity_button">
@@ -62,22 +62,23 @@
                             </div>
                         </td>
                         <td class="cart_total">
-                            <p class="cart_total_price">$59</p>
+                            <p class="cart_total_price">100 грн</p>
                         </td>
                         <td class="cart_delete">
                             <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
                         </td>
                     </tr>
+
                     <tr>
                         <td class="cart_product">
                             <a href=""><img src="{{ asset('images/cart/three.png') }}" alt=""></a>
                         </td>
                         <td class="cart_description">
-                            <h4><a href="">Colorblock Scuba</a></h4>
+                            <h4><a href="">тут короткий опис виробу</a></h4>
                             <p>Web ID: 1089772</p>
                         </td>
                         <td class="cart_price">
-                            <p>$59</p>
+                            <p>200 грн</p>
                         </td>
                         <td class="cart_quantity">
                             <div class="cart_quantity_button">
@@ -87,7 +88,7 @@
                             </div>
                         </td>
                         <td class="cart_total">
-                            <p class="cart_total_price">$59</p>
+                            <p class="cart_total_price">200 грн</p>
                         </td>
                         <td class="cart_delete">
                             <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
@@ -101,48 +102,48 @@
 <!--/#cart_items-->
 <section id="do_action">
     <div class="container">
-        <h4 style="text-align: center;"> Здесь Вы можете посчитать стоимость товаров в корзине включая доставку:</h4>
+        <h4 style="text-align: center;"> Тут Ви можете порахувати вартість товарів у кошику, включаючи доставку:</h4>
         <div class="row">
             <div class="col-sm-12">
                 <div class="total_area">
                     <ul>
-                        <li>Итого в корзине<span>500 грн </span></li>
+                        <li>Разом у кошику<span>800 грн </span></li>
                         <li>
                             <div class="row">
                                 <div class="col-sm-4">
                                     <input type="radio" id="shipping_1" name="contact" value="">
-                                    <label style="margin-right: 20px;" for="shipping_1"> Самовывоз</label>
+                                    <label style="margin-right: 20px;" for="shipping_1"> Самовивіз</label>
                                 </div>
 
                                 <div class="col-sm-4">
                                     <input type="radio" id="shipping_2" name="contact" value="">
-                                    <label style="margin-right: 20px;" for="shipping_2"> Курьером по Киеву</label>
+                                    <label style="margin-right: 20px;" for="shipping_2"> Кур'єром по Києву</label>
                                 </div>
 
                                 <div class="col-sm-4">
                                     <input type="radio" id="shipping_3" name="contact" value="">
-                                    <label for="shipping_3"> Почтой по Украине</label>
+                                    <label for="shipping_3"> Поштою по Україні</label>
                                 </div>
                             </div>
 
                         </li>
                         <li class="single_field">
                             <select>
-                                <option>Голосеевский</option>
-                                <option>Святошинский</option>
-                                <option>Соломенский</option>
-                                <option>Оболонский</option>
-                                <option>Подольский</option>
-                                <option>Печерский</option>
-                                <option>Шевченковский</option>
-                                <option>Дарницкий</option>
-                                <option>Днепровский</option>
-                                <option>Деснянский</option>
+                                <option>Голосіївський</option>
+                                <option>Святошинський</option>
+                                <option>Солом'янський</option>
+                                <option>Оболонський</option>
+                                <option>Подільський</option>
+                                <option>Печерський</option>
+                                <option>Шевченківський</option>
+                                <option>Дарницький</option>
+                                <option>Дніпровський</option>
+                                <option>Деснянський</option>
                             </select>
                         </li>
 
-                        <li>Доставка<span>0 грн</span></li>
-                        <li>Всего <span>500 грн </span></li>
+                        <li>Доставка<span>100 грн</span></li>
+                        <li>Усього, разом з доставкою <span>900 грн </span></li>
                     </ul>
                     <div style="text-align: center;">
                         <a class="btn btn-default update" href="">Очистить</a>
@@ -154,7 +155,7 @@
 
         <div class="recommended_items">
             <!--recommended_items-->
-            <h2 class="title text-center">Рекомендуемые товары</h2>
+            <h2 class="title text-center">рекомендовані товари</h2>
 
             <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
@@ -164,9 +165,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend1.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
@@ -177,9 +178,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend2.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
@@ -190,9 +191,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend3.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
@@ -205,9 +206,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend1.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
@@ -218,9 +219,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend2.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
@@ -231,9 +232,9 @@
                                 <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/home/recommend3.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <h2>500 грн</h2>
+                                        <p>Опис виробу</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i> Додати в кошик</a>
                                     </div>
 
                                 </div>
