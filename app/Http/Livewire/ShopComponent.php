5c8797b16446c33a933e93f4f22c071dbd343cc8
Anatolii <gurov.anatoliy@gmail.com>
2022-05-06 17:08:31 +0300

    Привязка боковой панели категории на стр Shop

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -5,13 +5,16 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Product;
+use App\Models\Category;
+use App\Models\Brand;
 use Cart;
 
 class ShopComponent extends Component
 {
     use WithPagination;
 
-    public function store_to_cart($product_id, $product_name, $product_price) {
+    public function store_to_cart($product_id, $product_name, $product_price)
+    {
         Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product'); // 1- форматирование денежных едениц(0/1)
         session()->flash('message', 'Вибраний виріб додано до кошику!');
         return redirect()->route('product.cart');
@@ -19,8 +22,14 @@ class ShopComponent extends Component
 
     public function render()
     {
-        $products = Product::orderBy('updated_at', 'DESC')->paginate(9);
+        $brands = Brand::get();
+        $categories = Category::get();
+        $products = Product::orderBy('updated_at', 'DESC')->paginate(6);
 
-        return view('livewire.shop-component', ['products' => $products])->layout('layouts.base');
+        return view('livewire.shop-component', [
+            'products' => $products,
+            'categories' => $categories,
+            'brands' => $brands,
+        ])->layout('layouts.base');
     }
 }
