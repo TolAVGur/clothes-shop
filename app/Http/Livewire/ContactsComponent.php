4c991cf226f1da48fe0ff482e26e41118d8a9082
Anatolii <gurov.anatoliy@gmail.com>
2022-05-29 14:28:36 +0300

    Надсилання пошти зі сторінки контактів (feedback)

--- app/Http/Livewire/ContactsComponent.php
+++ app/Http/Livewire/ContactsComponent.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\FeedbackMail;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class ContactsComponent extends Component
 {
+    public $name;                   // ім'я
+    public $email;                  // email
+    public $subject;                // тема повідомлення
+    public $message;                // текст повідомлення
+
+    public function feedbackMail() {
+        $name = $this->name;
+        $email = $this->email;
+        $subject = $this->subject;
+        $message = $this->message;
+        Mail::to('clothes.shop@domain.com')->send(new FeedbackMail($name, $email, $subject, $message));
+        return redirect()->route('feedback');
+    }
+
     public function render()
     {
         return view('livewire.contacts-component')->layout('layouts.base');
